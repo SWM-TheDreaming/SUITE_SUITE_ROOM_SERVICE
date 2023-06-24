2e7009b80b68fcf71bb64eba19dae9692406cb28
김대현 <darren@gimdaehyeon-ui-MacBookAir.local>
2023-06-24 21:10:54 +0900

    init suite-user-service

---
+++ src/main/java/com/suite/suite_user_service/SuiteUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suite.suite_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuiteUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suite/suite_user_service/SuiteUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suite.suite_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
