42c6ac418e4cf12aa2ea170e907e976768b2994b
반영환 <lopahn2@gmail.com>
2023-07-24 12:46:07 +0900

    fetch: init project

---
+++ src/main/java/com/example/suite_suite_room/SuiteSuiteRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.suite_suite_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteSuiteRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuiteSuiteRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suite_suite_room/SuiteSuiteRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suite_suite_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteSuiteRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
