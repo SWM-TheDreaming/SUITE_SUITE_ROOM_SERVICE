9af088da8abdc35df7c39ae9816d7f81deff088c
김대현 <zxz4641@gmail.com>
2023-08-02 16:43:05 +0900

    test: [스위트룸 참가하기[C] 스위트룸 참가하기 컨트롤러 테스트

--- src/main/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantController.java
+++ src/main/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantController.java
@@ -1,11 +1,14 @@
 package com.suite.suite_suite_room_service.suiteRoom.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/participant")
+@RequestMapping("/suite")
 public class ParticipantController {
+
+    //@PostMapping("/suiteroom/attend")
 }
--- src/test/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantControllerTest.java
+++ src/test/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantControllerTest.java
@@ -3,10 +3,10 @@ package com.suite.suite_suite_room_service.suiteRoom.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.suite.suite_suite_room_service.suiteRoom.dto.Message;
 import com.suite.suite_suite_room_service.suiteRoom.dto.ReqSuiteRoomDto;
-import com.suite.suite_suite_room_service.suiteRoom.dto.ReqUpdateSuiteRoomDto;
-import com.suite.suite_suite_room_service.suiteRoom.dto.ResSuiteRoomDto;
+import com.suite.suite_suite_room_service.suiteRoom.entity.SuiteRoom;
 import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockAuthorizer;
 import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockSuiteRoom;
+import com.suite.suite_suite_room_service.suiteRoom.service.ParticipantServiceImpl;
 import com.suite.suite_suite_room_service.suiteRoom.service.SuiteRoomServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,53 +18,34 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SuiteRoomControllerTest {
+class ParticipantControllerTest {
 
     @Autowired private ObjectMapper mapper;
     @Autowired private MockMvc mockMvc;
+    @Autowired private ParticipantServiceImpl participantService;
     @Autowired private SuiteRoomServiceImpl suiteRoomService;
+
     public static final String YH_JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb3BhaG4yQGdtYWlsLmNvbSIsIklEIjoiMSIsIk5BTUUiOiLrsJjsmIHtmZgiLCJOSUNLTkFNRSI6Imh3YW55OTkiLCJBQ0NPVU5UU1RBVFVTIjoiQUNUSVZBVEUiLCJST0xFIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjkwODE0MDk5LCJleHAiOjE2OTE0MTg4OTl9.xlbiPnD366KP49g5v6X_F4yEZRvu6rbf3ph6j-AWxs8";
     public static final String DR_JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6eHo0NjQxQGdtYWlsLmNvbSIsIklEIjoiMiIsIk5BTUUiOiLquYDrjIDtmIQiLCJOSUNLTkFNRSI6IkRhcnJlbiIsIkFDQ09VTlRTVEFUVVMiOiJBQ1RJVkFURSIsIlJPTEUiOiJST0xFX1VTRVIiLCJpYXQiOjE2OTA4MTQxMzUsImV4cCI6MTY5MTQxODkzNX0.ob5s7qQxdALJHpxb28pyYFiAqdeifGKfxtGx9MD3KQ0";
-    @Test
-    @DisplayName("스위트룸 생성")
-    public void createSuiteRoom() throws Exception {
-        //given
-        ReqSuiteRoomDto reqSuiteRoomDto = MockSuiteRoom.getMockSuiteRoom("title2", true);
-        String body = mapper.writeValueAsString(reqSuiteRoomDto);
-        //when
-        String responseBody = postRequest("/suite/suiteroom/registration", DR_JWT, body);
-        Message message = mapper.readValue(responseBody, Message.class);
-        //then
-        Assertions.assertAll(
-                () -> assertThat(message.getStatusCode()).isEqualTo(200)
-        );
-
-        System.out.println(responseBody);
-
-    }
 
     @Test
-    @DisplayName("스위트룸 수정")
-    public void renewalSuiteRoom() throws Exception {
+    @DisplayName("스위트룸 참가하기")
+    public void joinSuiteRoom() throws Exception {
         //given
-        final String url = "/suite/suiteroom/update";
         ReqSuiteRoomDto reqSuiteRoomDto = MockSuiteRoom.getMockSuiteRoom("title2", true);
         suiteRoomService.createSuiteRoom(reqSuiteRoomDto, MockAuthorizer.getMockAuthorizer("hwany"));
-        String body = mapper.writeValueAsString(ReqUpdateSuiteRoomDto.builder()
-                .suiteRoomId(Long.parseLong("1"))
-                .content("updated content")
-                .channelLink("http://www.naver.com").build());
+        String body = mapper.writeValueAsString(new HashMap<String, Long>().put("suiteRoomId", Long.parseLong("1")));
         //when
-        String responseBody = patchRequest(url, YH_JWT, body);
+        String responseBody = postRequest("/suite/suiteroom/attend", DR_JWT, body);
         Message message = mapper.readValue(responseBody, Message.class);
         //then
         Assertions.assertAll(
@@ -72,48 +53,7 @@ class SuiteRoomControllerTest {
         );
     }
 
-    @Test
-    @DisplayName("스위트룸 그룹 목록 확인")
-    public void listUpSuiteRooms() throws Exception {
-        //given
-        final String url = "/suite/suiteroom/";
 
-        ReqSuiteRoomDto reqSuiteRoomDto1 = MockSuiteRoom.getMockSuiteRoom("hwany", true);
-        ReqSuiteRoomDto reqSuiteRoomDto2 = MockSuiteRoom.getMockSuiteRoom("mini", true);
-        suiteRoomService.createSuiteRoom(reqSuiteRoomDto1, MockAuthorizer.getMockAuthorizer("hwany"));
-        suiteRoomService.createSuiteRoom(reqSuiteRoomDto2, MockAuthorizer.getMockAuthorizer("hwany"));
-
-        List<ResSuiteRoomDto> resService = suiteRoomService.getAllSuiteRooms(MockAuthorizer.getMockAuthorizer("hwany"));
-        //when
-        String responseBody = getRequest(url, YH_JWT);
-        Message message = mapper.readValue(responseBody, Message.class);
-        List<ResSuiteRoomDto> result = (List<ResSuiteRoomDto>) message.getData();
-
-        //then
-        if(result.get(0) instanceof ResSuiteRoomDto) {
-            Assertions.assertAll(
-                    () -> assertThat(result.get(0)).isEqualTo(resService.get(0))
-            );
-        }
-
-    }
-
-    @Test
-    @DisplayName("스터디 파투")
-    public void deleteSuiteRoom() throws Exception {
-        //given
-        final String url = "/suite/suiteroom/delete/1";
-        ReqSuiteRoomDto reqSuiteRoomDto = MockSuiteRoom.getMockSuiteRoom("title2", true);
-        suiteRoomService.createSuiteRoom(reqSuiteRoomDto, MockAuthorizer.getMockAuthorizer("hwany"));
-        //when
-        String responseBody = deleteRequest(url, YH_JWT);
-        Message message = mapper.readValue(responseBody, Message.class);
-        //then
-        Assertions.assertAll(
-                () -> assertThat(message.getStatusCode()).isEqualTo(200)
-        );
-
-    }
 
     private String postRequest(String url, String jwt, String body) throws Exception {
         MvcResult result = mockMvc.perform(post(url)
