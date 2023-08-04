3d78315128eeb5a1d052b572a16cc9f20b79d517
반영환 <lopahn2@gmail.com>
2023-08-04 15:15:01 +0900

    test: [스터디원 납부상태 변경][C] 컨트롤러 테스트 코드 작성

--- src/test/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantControllerTest.java
+++ src/test/java/com/suite/suite_suite_room_service/suiteRoom/controller/ParticipantControllerTest.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.suite.suite_suite_room_service.suiteRoom.dto.Message;
 import com.suite.suite_suite_room_service.suiteRoom.dto.ReqSuiteRoomDto;
 import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockAuthorizer;
-import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockCheckInDto;
+import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockCheckInInfo;
 import com.suite.suite_suite_room_service.suiteRoom.mockEntity.MockSuiteRoom;
 import com.suite.suite_suite_room_service.suiteRoom.repository.SuiteRoomRepository;
 import com.suite.suite_suite_room_service.suiteRoom.security.dto.AuthorizerDto;
@@ -38,14 +38,13 @@ class ParticipantControllerTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private ParticipantServiceImpl participantService;
     @Autowired private SuiteRoomServiceImpl suiteRoomService;
-    @Autowired private SuiteRoomRepository suiteRoomRepository;
+
 
 
     public static final String YH_JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb3BhaG4yQGdtYWlsLmNvbSIsIklEIjoiMSIsIk5BTUUiOiLrsJjsmIHtmZgiLCJOSUNLTkFNRSI6Imh3YW55OTkiLCJBQ0NPVU5UU1RBVFVTIjoiQUNUSVZBVEUiLCJST0xFIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjkwODE0MDk5LCJleHAiOjE2OTE0MTg4OTl9.xlbiPnD366KP49g5v6X_F4yEZRvu6rbf3ph6j-AWxs8";
     public static final String DR_JWT = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6eHo0NjQxQGdtYWlsLmNvbSIsIklEIjoiMiIsIk5BTUUiOiLquYDrjIDtmIQiLCJOSUNLTkFNRSI6IkRhcnJlbiIsIkFDQ09VTlRTVEFUVVMiOiJBQ1RJVkFURSIsIlJPTEUiOiJST0xFX1VTRVIiLCJpYXQiOjE2OTA4MTQxMzUsImV4cCI6MTY5MTQxODkzNX0.ob5s7qQxdALJHpxb28pyYFiAqdeifGKfxtGx9MD3KQ0";
     @BeforeEach
     public void setUp() {
-        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         ReqSuiteRoomDto reqSuiteRoomDto = MockSuiteRoom.getMockSuiteRoom("title2", true);
         suiteRoomService.createSuiteRoom(reqSuiteRoomDto, MockAuthorizer.getMockAuthorizer("hwany", 1L));
     }
@@ -90,10 +89,9 @@ class ParticipantControllerTest {
         //given
         final String url = "/suite/payment/completion";
 
-        Map<String, MockCheckInDto> messageCard = new HashMap<String, MockCheckInDto>();
-        MockCheckInDto check = new MockCheckInDto(1L, 1L);
-        messageCard.put("checkInInfo", check);
-        System.out.println(messageCard);
+        Map<String, MockCheckInInfo> messageCard = new HashMap<String, MockCheckInInfo>();
+        MockCheckInInfo checkInInfo = new MockCheckInInfo(1L, 1L);
+        messageCard.put("checkInInfo", checkInInfo);
         String body = mapper.writeValueAsString(messageCard);
         //when
         String responseBody = postRequest(url, YH_JWT, body);
