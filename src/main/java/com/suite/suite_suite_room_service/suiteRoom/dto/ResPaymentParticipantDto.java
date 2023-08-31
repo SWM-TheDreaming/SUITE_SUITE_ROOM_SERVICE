bd2ca19039e184a65f90407080cadf5c92092ed4
반영환 <lopahn2@gmail.com>
2023-08-31 14:57:43 +0900

    feat: 계약서 시작 완료 - 카프카 붙이기 완료

--- src/main/java/com/suite/suite_suite_room_service/suiteRoom/dto/ResPaymentParticipantDto.java
+++ src/main/java/com/suite/suite_suite_room_service/suiteRoom/dto/ResPaymentParticipantDto.java
@@ -8,12 +8,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ResPaymentParticipantDto {
     private String nickName;
+    private String email;
     private SuiteStatus status;
     private boolean isHost;
     @Builder
-    public ResPaymentParticipantDto(String nickName, SuiteStatus status, boolean isHost) {
+    public ResPaymentParticipantDto(String nickName, String email, SuiteStatus status, boolean isHost) {
         this.nickName = nickName;
+        this.email = email;
         this.status = status;
         this.isHost = isHost;
+
     }
 }
